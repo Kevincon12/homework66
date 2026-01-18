bf23d9a9c4a5cd7e4e3ac2c536b3ef290e9a44b2
Strelnikov Ilia <ily_strelnikov@mail.ru>
2026-01-18 13:13:31 +0600

    Добавили вывод всей информации с API на страницу

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Home from "./containers/Home/Home.tsx";
 
 const App = () => {
 
 
   return (
     <>
-
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="*" element={<h1 className="text-center mt-5">Not found</h1>} />
+        </Routes>
     </>
   )
 };
--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,10 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import type {Meal} from "../../types";
 import axiosApi from "../../axiosAPI.ts";
+import {useNavigate} from "react-router-dom";
 
 const Home = () => {
     const [loading, setLoading] = useState(false);
     const [meals, setMeals] = useState<Meal[]>([]);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         const showMeals = async () => {
@@ -26,16 +29,42 @@ const Home = () => {
         showMeals();
     }, []);
 
+    const totalCalories = meals.reduce((sum, meal) => sum + meal.calories, 0);
+
     return (
-        <div>
+        <div className='container mt-2'>
             <h1 className='card-title'>Calorie Tracker</h1>
-            <p className='card-text'>Total calories: </p>
-            <button className='btn btn-primary'>Add new meal</button>
+            <hr/>
+            <p className='card-text'>Total calories: {totalCalories}</p>
+            <button
+                className='btn btn-primary mb-3'
+                onClick={() => navigate('/meals/newMeal')}
+            >Add new meal</button>
 
             {loading && (
                 <div className="text-center my-3">
                     <div className="spinner-border text-primary" role="status">
                         <span className="visually-hidden">Sending</span>
+                    </div>
+                </div>
+            )}
+
+            {meals.length > 0 && (
+                <div className='container'>
+                    <div className='row'>
+                        {meals.map((meal) => (
+                            <div key={meal.id} className='col-md-4 mb-2'>
+                                <div className='card h-100 shadow-sm'>
+                                    <div className='card-body d-flex flex-column justify-content-between'>
+                                        <h3 className='card-title'>{meal.type}</h3>
+                                        <p className="card-text">{meal.description}</p>
+                                        <p className="card-text">Calories: {meal.calories}</p>
+                                        <button type='button' className='btn btn-primary mb-2'>Edit</button>
+                                        <button type='button' className='btn btn-warning'>Delete</button>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             )}
